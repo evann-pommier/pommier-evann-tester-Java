cd6845d3f0eb06772683b2f2236631774b834b43
Neylle <evannpommier64@gmail.com>
2026-01-30 12:35:54 +0100

    Correction d'erreur de division dans FareCalculatorService.java et mise à jour de JaCoCo et Surefire

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,27 +5,24 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
+	public void calculateFare(Ticket ticket) {
+	    if (ticket.getOutTime() == null || ticket.getOutTime().before(ticket.getInTime())) {
+	        throw new IllegalArgumentException("Out time provided is incorrect: " + ticket.getOutTime());
+	    }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+	    long durationMillis = ticket.getOutTime().getTime() - ticket.getInTime().getTime();
+	    double durationHours = durationMillis / (1000.0 * 60 * 60); // durée en heures (double)
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+	    switch (ticket.getParkingSpot().getParkingType()) {
+	        case CAR:
+	            ticket.setPrice(durationHours * Fare.CAR_RATE_PER_HOUR);
+	            break;
+	        case BIKE:
+	            ticket.setPrice(durationHours * Fare.BIKE_RATE_PER_HOUR);
+	            break;
+	        default:
+	            throw new IllegalArgumentException("Unknown Parking Type");
+	    }
+	}
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        }
-    }
 }
